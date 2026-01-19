b37d7832b209b78e99c169c0e26b881945973a8f
ОБПОУ КГПК <kgpk2@outlook.com>
2026-01-19 14:59:05 +0300

    Родительский проект создан, но не до конца. (Остановился на AuthPage.xaml)

---
+++ Park/Park/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Park
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Model.ParkContext Context { get; set; } = new Model.ParkContext();
+
+        public static Model.User CurrrentUser { get; set; } = null;
+    }
+
+}
---
+++ Park/Park/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Park
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Pages.AuthPage());
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            Page page = MainFrame.Content as Page;
+
+            if (page.Title == "Авторизация")
+            {
+                BtnBack.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                BtnBack.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void BtnBack_Back(object sender, RoutedEventArgs e)
+        {
+            MainFrame.GoBack();
+        }
+    }
+}
